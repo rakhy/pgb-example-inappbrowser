191e947ed742e79729e537d4442a8f06291fb443
rakhy <rakhy.in@gmail.com>
2016-07-01 18:28:57 +0530

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        var ref = window.open('http://apache.org', '_blank', 'location=yes');
+        var ref = window.open('http://www.youtube.com/embed/urmU1XIzW6g?html5=1', '_blank', 'location=yes');
 
         ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
         ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
